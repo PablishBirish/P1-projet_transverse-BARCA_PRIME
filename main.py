8be4bb156f2f3dbf86d103853d2b18449e10b47d
Pablo BIRAN <python.afton.36@gmail.com>
2025-03-03 15:15:50 +0100

    .

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import pygame
 
-# Initialisation des variables
-running = True          # Cette variable contient l'état de fonctionnement du jeu ('True' s'il tourne, 'False' s'il ne tourne pas)
-screen = pygame.display.set_mode((1280, 720))
+from Game import Game
 
-# Mise en place de la fenêtre
-pygame.display.set_mode((800, 600))         # On délimite la taille de la fenêtre
-pygame.display.set_caption("Projet Transverse - V1")            # On donne un nom à la fenêtre
+if __name__ == "__main__":
+    pygame.init()
+    game = Game()
+    game.run()
 
-while running :             # Cette boucle fera tourner le jeu tant que la variable 'running' est égal à 'True'
-    for event in pygame.event.get():                # On récupère la liste des évènements actifs
-        if event.type == pygame.QUIT :          # On récupère et vérifie le type d'évènement
-            running = False             # Si le joueur tente de fermer la fenêtre (le type d'évènement est le fait de quitter), le programme sort de la boucle while
 pygame.quit()

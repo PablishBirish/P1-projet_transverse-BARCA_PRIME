453d846dd00bc58280c5b7295d5017240d1b5ce1
Pablo BIRAN <python.afton.36@gmail.com>
2025-03-03 14:39:21 +0100

    .

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import pygame
 
-print("Hello World !")
+# Initialisation des variables
+running = True          # Cette variable contient l'état de fonctionnement du jeu ('True' s'il tourne, 'False' s'il ne tourne pas)
+screen = pygame.display.set_mode((1280, 720))
